637a36fea06cf0f452956f304e32d2c81fc99e98
Farzad Jpt <farzad.jafarpour@gmail.com>
2022-02-26 20:11:10 +0330

    added cors middleware to the project

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const winston = require("winston");
 const express = require("express");
 const app = express();
+const corsMiddleware = require("./middleware/cors");
+
+app.options("*", corsMiddleware);
+app.use(corsMiddleware);
 
 require("./startup/logging");
 require("./startup/db")();
 require("./startup/routes")(app);
 require("./startup/config");
-const corsMiddleware = require("./middleware/cors");
-
-app.options("*", corsMiddleware);
-app.use(corsMiddleware);
 
 const server = app.listen(3900, () => winston.info("Listening on port 3000"));
 
--- middleware/cors.js
+++ middleware/cors.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
+  AccessControlAllowOrigin: "https://example.com",
+  AccessControlAllowCredentials: true,
 };
 
 module.exports = cors(corsOptions);
